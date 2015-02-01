b4cfd4d40c9b79dacf175f0afcbebe2634fb3128
Adam Koprowski <koper@google.com>
2015-01-31 19:48:20 -0800

    Added master-detail navigation for the catalogue.

--- js/all.js
+++ js/all.js
@@ -9,24 +9,45 @@ angular.module('chwasty', ['ngRoute', 'ngAnimate'])
           templateUrl: 'catalogue.html',
           controller: 'CatalogueCtrl'
         })
+        .when('/katalog/:plantId', {
+          templateUrl: 'plant.html',
+          controller: 'PlantCtrl'
+        })
         .otherwise('/');
       $locationProvider.html5Mode(false);
   }])
 
   .controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
     $scope.menuItems = [
-      { href: '#/', text: 'Strona główna' },
-      { href: '#/katalog', text: 'Katalog chwastów' }
+      { href: '/#/', text: 'Strona główna' },
+      { href: '/#/katalog', text: 'Katalog chwastów' }
     ];
     $scope.isActive = function(menuItem) {
-      return _.endsWith($location.absUrl(), menuItem.href);
+      var url = '/#' + $location.path();
+      var itemUrl = menuItem.href;
+      // Strict matching for the main location; prefix-matching for the rest.
+      if (url == '/#/' || itemUrl == '/#/') {
+        return url == itemUrl;
+      } else {
+        return _.startsWith(url, itemUrl);
+      }
     };
   }])
 
   .controller('CatalogueCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.plants = [];
 
+    $scope.getDetailsUrl = function(plantId) {
+      return '/#/katalog/' + plantId;
+    };
+
     $http.get('data/plants.json').success(function(data) {
       $scope.plants = data;
+    });
+  }])
+
+  .controller('PlantCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $http.get('data/plants.json').success(function(data) {
+      $scope.plant = data[$routeParams.plantId];
     });
   }]);
